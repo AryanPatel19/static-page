9b456161b5e8bf4c09cab21fdfe81cff91e7afb5
AryanPatel19 <aryanmathukiya@gmail.com>
2023-04-26 22:53:54 +0530

    commit as aws

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+</head>
+<body>
+	<h1>Welcome to My Website</h1>
+	<p>This is my single page website built with PHP.</p>
+	<p>Here is some more content.</p>
+	<ul>
+		<li>Item 1</li>
+		<li>Item 2</li>
+		<li>Item 3</li>
+	</ul>
+	<p>Thanks for visiting!</p>
+</body>
+</html>
